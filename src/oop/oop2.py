5eccc828d6ed0bff9979349c0ef5dbc49ddc65bd
Reinoso <jreinoso@amazon.com>
2020-08-14 20:34:34 -0400

    finished oop2.py, but will have to relook to as of why test_oop2.ppy is giving me issues

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,17 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # TODO
+    def drive(self):
+        print("vroooom")
+
+    def __str__(self):
+        return f'{self.num_wheels}'    
+        
+
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -18,6 +25,15 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(num_wheels = 2)
+          
+    def drive(self):
+        print("BRAAAP!!")
+       
+
+
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +46,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for vehicleList in vehicles:
+    vehicleList.drive()
